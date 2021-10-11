e7c2501e47a4bdfb1fef29f1679fc27eeb84b806
Ademola Adebowale <fadebowaley@gmail.com>
2021-10-11 19:40:52 +0100

    update status

--- main.go
+++ main.go
@@ -1,25 +1,37 @@
 package main
+
 import (
-"fmt"
-"net/http"
+	"fmt"
+	"net/http"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-w.Header().Set("Content-Type", "text/html")
-if r.URL.Path == "/" {
-fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-} else if r.URL.Path == "/contact" {
-fmt.Fprint(w, "To get in touch, please send an email "+
-"to <a href=\"mailto:support@lenslocked.com\">"+
-"support@lenslocked.com</a>.")
-} else if  r.URL.Path == "/about" {
-fmt.Fprint(w, "<h1>This is about us page</h1>")
-}
-}
 
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my dozie </h1>")
 
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "<h1>Welcome to about us </h1>")
+
+	} else if r.URL.Path == "/service" {
+		fmt.Fprint(w, "<h1> see our services </h1>")
+
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email "+
+			"to <a href=\"mailto:support@lenslocked.com\">"+
+			"support@lenslocked.com</a>")
+
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you "+
+			"were looking for Error 404</h1>"+
+			"<p>Please email us if you keep being sent to an "+
+			"invalid page.</p>")
+	}
+}
 
 func main() {
-http.HandleFunc("/", handlerFunc)
-http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
